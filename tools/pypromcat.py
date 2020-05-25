4e7a3c8444cdff0aa62d87cd46b2efccb794e64d
David de Torres <david.detorres@sysdig.com>
2020-05-25 09:52:32 +0200

    Fixed prypromcat library to print yaml correctly

--- tools/pypromcat.py
+++ tools/pypromcat.py
@@ -1,6 +1,7 @@
 import yaml
 import json
 from yaml.representer import SafeRepresenter
+import re
 
 def getConfigurations(resource):
   return resource['configurations']
@@ -37,13 +38,33 @@ def prometheusAlert2SysdigAlert(promAlert):
 def dict2BeautyString(jsonToPrint):
   return json.dumps(jsonToPrint,indent=2)
 
+def removeTrailingSpaces(data):
+  data = re.sub(' +\n','\n',data)
+  data = re.sub(' +$','',data)
+  return data
+
+def removeTrailingSpacesFromAllElements(data):
+  if type(data) == dict: 
+    for element in data:  
+      if type(data[element]) == dict or type(data[element]) == list:
+        data[element] = removeTrailingSpacesFromAllElements(data[element])
+      elif type(data[element]) == str: 
+        data[element] = removeTrailingSpaces(data[element])
+  if type(data) == list:
+    for idx in range(len(data)):
+      if type(data[idx]) == dict or type(data[idx]) == list:
+        data[idx] = removeTrailingSpacesFromAllElements(data[idx])
+      elif type(data[idx]) == str: 
+        data[idx] = removeTrailingSpaces(data[idx])
+  return data
+
 def dict2BeautyYaml(yaml2print):
   def str_presenter(dumper, data):
     if len(data.splitlines()) > 1:  # check for multiline string
       return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
     return dumper.represent_scalar('tag:yaml.org,2002:str', data)
   yaml.add_representer(str, str_presenter)
-  return(yaml.dump(yaml2print))
+  return(yaml.dump(removeTrailingSpacesFromAllElements(yaml2print), sort_keys=False))
 
 def createArrayOfSysdigAlerts(file):
   alertsYaml = loadYamlFile(file)
