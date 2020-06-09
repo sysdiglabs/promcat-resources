1ccafa2d98253d791182af76d1f12ef6921d2e4e
David Lorite <david.lorite@sysdig.comm>
2020-06-09 14:26:09 +0200

    More info

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 kubectl version --client
 kubectl -n staging get po
+kubectl -n staging delete jobs.batch dbimport
+kubectl -n staging get po
 kubectl apply -f deployment/dbimport-job.yaml
+kubectl -n staging get po
