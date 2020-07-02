f5f51d3d9bb423229e0af86873fa3c20a0a68c0f
David Lorite <david.lorite@sysdig.comm>
2020-07-02 12:35:54 +0200

    Fix aws-lambda dashboard and add new workflows

--- deployment/staging/deploy.sh
+++ deployment/staging/deploy.sh
@@ -3,5 +3,5 @@ kubectl version --client
 kubectl -n staging get po
 kubectl -n staging delete jobs.batch dbimport
 kubectl -n staging get po
-kubectl apply -f deployment/dbimport-job.yaml
+kubectl apply -f dbimport-job.yaml
 kubectl -n staging get po
