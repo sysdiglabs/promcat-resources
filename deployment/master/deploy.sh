3cd0d208e0d5c926f434d15ef361818510d1926d
David Lorite <david.lorite@sysdig.com>
2021-08-17 11:14:06 +0200

    Fix deploy.sh

--- deployment/master/deploy.sh
+++ deployment/master/deploy.sh
@@ -3,5 +3,5 @@ kubectl version --client
 kubectl -n promhub get po
 kubectl -n promhub delete jobs.batch dbimport
 kubectl -n promhub get po
-kubectl apply -f dbimport-job.yaml
+kubectl apply -f deployment/master/dbimport-job.yaml
 kubectl -n promhub get po
